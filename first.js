7ae9ef4c7eb1efa3a9593febbdb7834142aa3971
Asad1raza <asraza311@gmail.com>
2025-07-29 20:55:03 +0500

    fixed

---
+++ first.js
@@ -0,0 +1,172 @@
+// let product = [{
+//     title: "Ball pen",
+//     rating: "4",
+//     price: "270,"
+// },
+// {
+//     title: "Ball pen",
+//     rating: "4",
+//     price: "270,"
+// }];
+
+// let data = [{
+//     title: "data 1",
+//     rating: "4",
+//     price: "270,"
+// },
+// {
+//     title: "data 2",
+//     rating: "4",
+//     price: "270,"
+// }];
+
+// let num = [1,2,3,4,5]
+
+
+// console.log([...product,...data]);
+
+
+// const profile = {
+//     username:"yourfvrtbatman",
+//     follower: 290,
+//     isFollow: true,
+//     following: 1,
+// };
+// console.log(typeof profile["isFollow"])
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a = 5, "b =", b = 2, );
+// console.log("a + b = ", a + b,);
+// console.log("a - b =", a - b, );
+// console.log("a / b =", a / b, );
+// console.log("a ** b =", a ** b, );
+// console.log("a % b =", a % b, );
+
+// // Unary Operatoer
+
+
+// console.log(a++);
+
+
+// console.log(a)
+
+
+// console.log("--a =", --a,);
+// console.log("a++ =", a++,);
+// console.log("++a =", ++a,);
+// let a = 5;
+// let b = 2;
+
+// console.log("a =", a = 5, "b =", b = 2, );
+
+
+// let a = 4;
+
+// a -= 6;
+
+
+// console.log(a);
+
+// a -= 4;
+// console.log("a =", a);
+
+// a **= 5;
+// console.log("a =", a );
+
+// console.log("a!=b", a!=b)
+
+// let a = 8;
+// let b = 3;
+
+// let cond1 = a > b ;
+
+//
+
+// console.log(check)
+// console.log("a && b =", cond1 && cond2);
+
+// let age = 19;
+
+// if(age > 18) {
+//     console.log("You Can vote Now!")
+// }
+// if(age < 18) {
+//     console.log("You Cannot vote!")
+// }
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color = "Black";
+// }
+// if(mode === "light"){
+//     color = "white"
+// }
+// console.log(color);
+
+// let age = "17b   ";
+
+// if(age > "18"){
+//     console.log("Vote")
+// }else{
+//     console.log("Cannot vote")
+// }
+ 
+// let num = "9";
+ 
+// if(num%2 === 0 ){
+//      console.log("even")
+//  }else{
+//      console.log("odd")
+//  }
+//  console.log("Asad")
+
+// const accountId = 112233
+// let accountEmail = "asraza311@gmail.com"
+// var accountPassword = "asadraza"
+// // accountCity = "Karachi"
+
+// //  console.log(accountId);
+
+// // console.table([accountId, accountEmail, accountPassword])
+
+//  accountEmail = "as3112@gmail.xyz"
+//  accountPassword = "009900"
+// // console.table([accountId, accountEmail, accountPassword])
+
+//  let score = false
+
+//  //console.log(typeof score);
+
+// //  let valueinNumber = Number(score)
+
+//  let isLoggedIn = 0
+
+//  let booleaninLoggedIn = Boolean(isLoggedIn)
+//  //console.log(isLoggedIn);
+
+// let value = 3
+
+// let negValue = -value
+// //console.log(negValue);
+
+//  let str1 = "Hello"
+//  let str2 = " Asad"
+//  let str3 = str1 + str2
+//  //console.log(str3);
+ 
+// //   console.log(" 1" + 2)
+// // console.log( 1 + "2")
+// //  console.log(" 1" + "2")
+// // console.log(1 + 2 + "3")
+
+// let gameCounter = 100
+// gameCounter--
+// console.log(gameCounter);
+
+
+
+
+
